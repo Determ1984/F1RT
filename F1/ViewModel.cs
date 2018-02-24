c675baa8bd7aef1c383b33e993ebed57538ea7c1
DETERM <determ1984@gmail.com>
2018-02-24 23:36:21 +0300

    Worring
    RelayCommand _ is not exist!!!

---
+++ F1/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace F1
+{
+    public class ViewModel
+    {   public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        private ICommand _calc;
+
+        public ICommand Calc
+        {
+            get
+            {
+                return _calc ?? (_calc =>(Re()=>Z=X+Y;))
+
+            }
+
+        }       
+    }
+}
